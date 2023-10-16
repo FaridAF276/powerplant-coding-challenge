81360b5f21815099f707cdaac125adb9e90c48c7
faridaf276 <farid-f32@msn.com>
2023-10-16 11:20:39 +0200

    exception handling

--- gems_powerplants/route.py
+++ gems_powerplants/route.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from utils import parse_body, get_powerplants
 from typing import List
 from output_models import PowerPlant
@@ -10,4 +10,7 @@ def root():
 @app.post("/productionplan")
 async def production(payload: PowerPlantPayload) -> List[PowerPlant]:
     power_plants_datas = parse_body(input=payload)
-    return await get_powerplants(powerplants=sorted(power_plants_datas, key=lambda x:x.cost_per_mwh), load=payload.load)
+    try: 
+        await get_powerplants(powerplants=sorted(power_plants_datas, key=lambda x:x.cost_per_mwh), load=payload.load)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=e.args)
